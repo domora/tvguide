03eeef515f928625f342a8592fb54eeebd7a04e6
Marc Blanc-Patin <marc.blancpatin@domora.com>
2014-04-28 11:56:48 +0200

    moved fixtures in separate file fixed Doctrine annotations issues

--- cli-config.php
+++ cli-config.php
@@ -4,4 +4,6 @@ require_once __DIR__.'/vendor/autoload.php';
 
 $app = new Domora\TvGuide\Application();
 
+\Doctrine\Common\Annotations\AnnotationRegistry::registerLoader('class_exists');
+
 return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($app['orm.em']);
--- web/index.php
+++ web/index.php
@@ -3,17 +3,12 @@
 $loader = require_once __DIR__.'/../vendor/autoload.php';
 require_once 'data.php';
 
-
 use Domora\TvGuide\Data\Program;
 use Domora\TvGuide\Data\Channel;
 
 Doctrine\Common\Annotations\AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
 $app = new Domora\TvGuide\Application();
-
-
-//~ $fixtures = new Domora\DataFixtures($app['orm.em']);
-//~ $fixtures->load();
 
 $app->mount('/v1', new Domora\TvGuide\Controller\ControllerProvider());
 $app->run();
