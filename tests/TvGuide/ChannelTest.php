29a9b092c2e122aad8880582b6af12ed5dd90a50
Pierre de Beaucorps <pierre.debeaucorps@domora.com>
2014-11-07 16:57:34 +0100

    add tests : GET on channels

--- tests/TvGuide/ChannelTest.php
+++ tests/TvGuide/ChannelTest.php
@@ -6,11 +6,46 @@ use Domora\Tests\WebTestCase;
 
 class ChannelControllerTest extends WebTestCase
 {
+    public function testGetChannelsList()
+    {
+        $client = $this->createClient();
+        $client->request('GET', '/v1/channels');
+
+        $response = $client->getResponse();
+        $this->assertTrue($response->isOk());
+
+        $json = json_decode($response->getContent(), true);
+        $this->assertTrue(is_array($json));
+    }
+
     public function testGetChannel()
     {
         $client = $this->createClient();
-        $crawler = $client->request('GET', '/v1/channels');
 
-        $this->assertTrue($client->getResponse()->isOk());
+        $channelId = 'fr-ch1';
+        $client->request('GET', '/v1/channels/' . $channelId);
+
+        $response = $client->getResponse();
+        $this->assertTrue($response->isOk());
+
+        $json = json_decode($response->getContent(), true);
+        $this->assertTrue(is_array($json));
+
+        $this->assertEquals($channelId, $json['id']);
+    }
+
+    public function testCreateProgramInChannel()
+    {
+        $client = $this->createClient();
+
+        // Create a program
+        $client->request('POST', '/v1/channels/fr-ch1/programs', [], [], ['content_type' => 'application/json'], json_encode([
+            'title' => 'Test Program',
+            'start' => 1415289600,
+            'stop' => 1415295600
+        ]));
+        
+        $response = $client->getResponse();
+        $this->assertTrue($response->isOk());
     }
 }
